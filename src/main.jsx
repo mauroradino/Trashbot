36675539d43a177203e4763a16a645a5c28a29bf
mauroradino <mauroradino2003@gmail.com>
2025-04-09 15:40:45 -0300

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from '../provider.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+    <Analytics />
     <App />
   </AuthProvider>
   </StrictMode>,
